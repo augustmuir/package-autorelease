37771ab6d56c0083105d59b55826b4d8c80f188f
augustkimo <august.kimo@icloud.com>
2023-04-29 13:10:13 -0400

    ex: more changes for v2

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export const hello = 'Hello, world! this will go in the first release. This will be in the second release/version.';
+export const hello = 'Hello, world! this will go in the first release. This will be in the second release/version. This will also be in v2!';
